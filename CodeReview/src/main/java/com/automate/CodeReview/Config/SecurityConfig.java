235ddc13df96340b45bb0134bb9c32d2b56d12d6
Natthapong Thongsukha <95947489+jajay456@users.noreply.github.com>
2025-08-22 10:46:45 +0700

    remove import

--- CodeReview/src/main/java/com/automate/CodeReview/Config/SecurityConfig.java
+++ CodeReview/src/main/java/com/automate/CodeReview/Config/SecurityConfig.java
@@ -3,8 +3,6 @@ package com.automate.CodeReview.Config;
 import com.automate.CodeReview.Filter.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -15,7 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import java.nio.charset.StandardCharsets;
 
 @Configuration
 @EnableMethodSecurity
