240df4966f718e3ecf4b3834aba74672e7d5ad10
HeisKong <162668675+HeisKong@users.noreply.github.com>
2025-08-28 16:18:51 +0700

    Update CommentDTO.java

--- CodeReview/src/main/java/com/automate/CodeReview/dto/CommentDTO.java
+++ CodeReview/src/main/java/com/automate/CodeReview/dto/CommentDTO.java
@@ -2,7 +2,7 @@ package com.automate.CodeReview.dto;
 
 
 public class CommentDTO {
-    private String comment;\
+    private String comment;
     public String getComment() { return comment; }
     public void setComment(String comment) { this.comment = comment; }
 
