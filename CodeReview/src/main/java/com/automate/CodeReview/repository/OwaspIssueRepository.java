99cedfbf12797d65b59c23f9dee099a029a1c7b8
Natthapong Thongsukha <95947489+jajay456@users.noreply.github.com>
2025-09-22 15:10:34 +0700

    new commit

--- CodeReview/src/main/java/com/automate/CodeReview/repository/OwaspIssueRepository.java
+++
@@ -1,9 +0,0 @@
-package com.automate.CodeReview.repository;
-import com.automate.CodeReview.entity.OwaspIssue;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface OwaspIssueRepository extends JpaRepository<OwaspIssue, Long> {
-    Optional<OwaspIssue> findByIssueKey(String issueKey);
-}
-
--- CodeReview/src/main/java/com/automate/CodeReview/repository/OwaspSummaryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.automate.CodeReview.repository;
-import com.automate.CodeReview.entity.OwaspSummary;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface OwaspSummaryRepository extends JpaRepository<OwaspSummary, Long> {
-    Optional<OwaspSummary> findByProjectKeyAndBranch(String projectKey, String branch);
-}
-
