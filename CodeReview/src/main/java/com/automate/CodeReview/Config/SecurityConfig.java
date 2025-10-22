5ac86aa9bbf8000b32c5a0e8119b6775a895ce14
Natthapong Thongsukha <95947489+jajay456@users.noreply.github.com>
2025-10-22 09:33:22 +0700

    new

--- CodeReview/src/main/java/com/automate/CodeReview/Config/SecurityConfig.java
+++ CodeReview/src/main/java/com/automate/CodeReview/Config/SecurityConfig.java
@@ -83,6 +83,7 @@ public class SecurityConfig {
                                 "/api/auth/login",
                                 "/api/auth/register"
                         ).permitAll()
+                        .requestMatchers("/api/scans/**").hasAnyRole("USER","ADMIN")
                         .requestMatchers("/api/sonar/logfile").permitAll()
                         .anyRequest().authenticated()
                 )
